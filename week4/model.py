6f0717d0076c454a56f56e100aa81212bc7fd1e0
Chulhan Lee <chulhan@corca.ai>
2024-02-25 21:43:39 +0900

    feat: rewrite model.py to improve readability.

--- week4/model.py
+++ week4/model.py
@@ -1,109 +1,90 @@
 import math
 from typing import Generator
+
 import torch
 from torch import Tensor, nn
+
 from constants import *
 
-# special thanks to 경택. 
+# Special thanks to 경택.
 class RecursiveDeviceModule(nn.Module):
+    """Module to recursively move all sub-modules to a specified device."""
     def to(self, device: str):
         self._device = device
         for module in self.children():
             module.to(device)
 
-class LookAheadMask(RecursiveDeviceModule):
-    def __init__(self):
-        super(LookAheadMask, self).__init__()
 
+class LookAheadMask(RecursiveDeviceModule):
+    """Mask to prevent attention to future tokens."""
     def forward(self, x: Tensor):
         x = x.clone().detach()
-        # Get upper triangular indices
         triu_indices = torch.triu_indices(x.size(1), x.size(1), offset=1)
-
-        # Set elements above the diagonal to 1
         for i in range(len(triu_indices[0])):
             idx_x, idx_y = triu_indices[:, i]
             x[:, idx_x.item(), idx_y.item()] = 1
         return x
 
+
 class PaddingMask(RecursiveDeviceModule):
+    """Mask to prevent attention to padding tokens."""
     def __init__(self, pad_idx: int):
         super(PaddingMask, self).__init__()
-        # Initialize parameters
-        self.pad_idx = PAD_IDX
+        self.pad_idx = pad_idx
 
     def forward(self, x: Tensor):
-        # x: (batch_size, max_sequence_length)
-        # Create mask by comparing with pad_idx and repeating it along the second dimension
         return torch.eq(x, self.pad_idx).unsqueeze(1).repeat(1, x.size(1), 1)
 
 
 class Attention(RecursiveDeviceModule):
+    """Scaled dot-product attention mechanism."""
     def __init__(self, d_model, num_heads, p_dropout):
         super(Attention, self).__init__()
-        # Initialize parameters
         self.d_model = d_model
         self.num_heads = num_heads
-        
-        # Calculate dimensionality of each head
         self.d_n = d_model // num_heads
-        
-        # Linear transformations for query, key, and value
         self.w_q = nn.Linear(d_model, self.d_n)
         self.w_k = nn.Linear(d_model, self.d_n)
         self.w_v = nn.Linear(d_model, self.d_n)
-        
-        # Softmax layer
         self.softmax = nn.Softmax(dim=-1)
         self.dropout = nn.Dropout(p=p_dropout)
 
-    def forward(self, q: Tensor, k: Tensor, v: Tensor, mask: Tensor):        
-        # Linear transformations
-        q = self.w_q(q)  # (batch_size, max_sequence_length, d_n)
-        k = self.w_k(k)  # (batch_size, max_sequence_length, d_n)
-        v = self.w_v(v)  # (batch_size, max_sequence_length, d_n)
-        
-        # Compute attention scores
+    def forward(self, q: Tensor, k: Tensor, v: Tensor, mask: Tensor):
+        q, k, v = self.w_q(q), self.w_k(k), self.w_v(v)
         attention_score = torch.matmul(q, torch.transpose(k, 1, 2)) / math.sqrt(self.d_n) + mask * -1e9
-        # (batch_size, max_sequence_length, max_sequence_length)
         attention_score = self.softmax(attention_score)
         return torch.matmul(self.dropout(attention_score), v)
 
 
 class MultiHeadAttention(RecursiveDeviceModule):
+    """Multi-head attention mechanism."""
     def __init__(self, d_model, num_heads, prob_attention_dropout, prob_residual_dropout):
         super(MultiHeadAttention, self).__init__()
-        # Initialize parameters
         self.d_model = d_model
         self.num_heads = num_heads
         self.d_n = d_model // num_heads
         self.attentions = nn.ModuleList([
             Attention(d_model, num_heads, prob_attention_dropout) for _ in range(num_heads)
         ])
-        
-        # Linear transformation for output
         self.residual_dropout = nn.Dropout(p=prob_residual_dropout)
         self.proj = nn.Linear(d_model, d_model)
 
     def forward(self, x: Tensor, mask: Tensor, encoder_output: Tensor | None = None):
-        attention_outputs: list[Tensor] = []
-        for attention in self.attentions:
-            attention_outputs.append(attention(x, x, x, mask))
+        attention_outputs = [attention(x, x, x, mask) for attention in self.attentions]
         attention_outputs = torch.cat(attention_outputs, dim=-1)
         return self.residual_dropout(self.proj(attention_outputs))
 
 
 class DecoderLayer(RecursiveDeviceModule):
+    """Decoder layer."""
     def __init__(self):
         super(DecoderLayer, self).__init__()
-
         self.attention = MultiHeadAttention(
             d_model=EMBEDDING_DIM,
             num_heads=HEAD,
             prob_attention_dropout=ATTENTION_DROPOUT,
             prob_residual_dropout=RESIDUAL_DROPOUT,
         )
-
         self.layer_norm_1 = nn.LayerNorm(EMBEDDING_DIM)
         self.layer_norm_2 = nn.LayerNorm(EMBEDDING_DIM)
         self.fc_1 = nn.Linear(EMBEDDING_DIM, 4 * EMBEDDING_DIM)
@@ -112,24 +93,19 @@ class DecoderLayer(RecursiveDeviceModule):
         self.dropout = nn.Dropout(RESIDUAL_DROPOUT)
 
     def forward(self, x: Tensor, mask: Tensor):
-        # x: (batch_size, seq_len, d_model)
-        # mask: (batch_size, seq_len, seq_len)
-
         x = x + self.attention(self.layer_norm_1(x), mask)
         x = x + self.dropout(self.proj(self.gelu(self.fc_1(self.layer_norm_2(x)))))
         return x
 
+
 class GPT(RecursiveDeviceModule):
+    """GPT model."""
     def __init__(self):
         super(GPT, self).__init__()
-        # Initialize parameters
         self.word_embedding = nn.Embedding(VOCAB_SIZE, EMBEDDING_DIM)
         self.position_embedding = nn.Embedding(BLOCK_SIZE, EMBEDDING_DIM)
         self.embedding_dropout = nn.Dropout(EMBEDDING_DROPOUT)
-        self.decoder_layers = nn.ModuleList(
-            [DecoderLayer() for _ in range(LAYER)]
-        )
-
+        self.decoder_layers = nn.ModuleList([DecoderLayer() for _ in range(LAYER)])
         self.layer_norm = nn.LayerNorm(EMBEDDING_DIM)
         self.fc_head = nn.Linear(EMBEDDING_DIM, VOCAB_SIZE, bias=False)
         self.block_size = BLOCK_SIZE
@@ -141,7 +117,6 @@ class GPT(RecursiveDeviceModule):
         self.adam_beta1 = ADAM_BETA1
         self.adam_beta2 = ADAM_BETA2
 
-
         self.apply(self._init_weights)
         for pn, p in self.named_parameters():
             if pn.endswith("proj.weight"):
@@ -150,7 +125,7 @@ class GPT(RecursiveDeviceModule):
                     mean=0.0,
                     std=self.weight_std / math.sqrt(2 * LAYER),
                 )
-    
+
     def _init_weights(self, module: nn.Module):
         if isinstance(module, nn.Linear):
             torch.nn.init.normal_(module.weight, mean=0.0, std=self.weight_std)
@@ -163,8 +138,7 @@ class GPT(RecursiveDeviceModule):
             torch.nn.init.ones_(module.weight)
 
     def get_optimizer(self):
-        decay_set: set[str] = set()
-        no_decay_set: set[str] = set()
+        decay_set, no_decay_set = set(), set()
         whitelist_weight_modules = (torch.nn.Linear,)
         blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
         for mn, m in self.named_modules():
@@ -198,14 +172,9 @@ class GPT(RecursiveDeviceModule):
             lr=self.lr,
         )
         return optimizer
-            
+
     def forward(self, x: Tensor):
-        # x: (batch_size, seq_len)
-
-        mask = self.look_ahead_mask(
-            self.padding_mask(x)
-        )  # (batch_size, seq_len, seq_len)
-
+        mask = self.look_ahead_mask(self.padding_mask(x))
         embeddings = self.word_embedding(x)  # (batch_size, seq_len, d_model)
 
         positions = torch.arange(embeddings.size(1), device=self._device).expand(
@@ -217,27 +186,21 @@ class GPT(RecursiveDeviceModule):
         x = self.embedding_dropout(
             embeddings + positions
         )  # (batch_size, seq_len, d_model)
-
         for decoder_layer in self.decoder_layers:
             x = decoder_layer(x, mask)
-        x = self.layer_norm(x)
-        return self.fc_head(x)  # (batch_size, seq_len, VOCAB_SIZE)
-    
+        return self.fc_head(self.layer_norm(x))
+
+    @torch.no_grad()
     def _generate_next_token(
         self, x: Tensor, temperature: float, top_k: int | None
     ) -> Tensor:
-        # x: (batch_size, seq_len)
-
-        logits = self.forward(x)  # (batch_size, seq_len, VOCAB_SIZE)
-        logits = logits[:, -1, :] / temperature  # (batch_size, VOCAB_SIZE)
-
+        logits = self.forward(x)
+        logits = logits[:, -1, :] / temperature
         if top_k is not None:
             logits_topk, _indices = logits.topk(top_k, dim=-1)
             logits[logits < torch.min(logits_topk)] = -1e9
             probs = torch.softmax(logits, dim=-1)
-
-            return torch.multinomial(probs, num_samples=1)  # (batch_size, 1)
-
+            return torch.multinomial(probs, num_samples=1)
         next_token = torch.argmax(logits, dim=-1)
         return next_token.unsqueeze(-1)
 
@@ -249,19 +212,14 @@ class GPT(RecursiveDeviceModule):
         temperature: float = 1.0,
         top_k: int | None = None,
     ) -> Tensor:
-        # x: (1, seq_len)
         if x.size(0) != 1:
-            raise ValueError("batch size should be 1")
+            raise ValueError("Batch size should be 1")
 
         if str(x.device) != self._device:
             x = x.to(self._device)
 
         for _ in range(max_iter):
-            x = (
-                x
-                if x.size(1) <= self.block_size
-                else x[:, -self.block_size :]
-            )
+            x = x if x.size(1) <= self.block_size else x[:, -self.block_size :]
             next_token = self._generate_next_token(x, temperature, top_k)
             x = torch.cat([x, next_token], dim=-1)
 
@@ -275,19 +233,14 @@ class GPT(RecursiveDeviceModule):
         temperature: float = 1.0,
         top_k: int | None = None,
     ) -> Generator[Tensor, None, None]:
-        # x: (1, seq_len)
         if x.size(0) != 1:
-            raise ValueError("batch size should be 1")
+            raise ValueError("Batch size should be 1")
 
         if str(x.device) != self._device:
             x = x.to(self._device)
 
         for _ in range(max_iter):
-            x = (
-                x
-                if x.size(1) <= self.block_size
-                else x[:, -self.block_size :]
-            )
+            x = x if x.size(1) <= self.block_size else x[:, -self.block_size :]
             next_token = self._generate_next_token(x, temperature, top_k)
             yield next_token
             x = torch.cat([x, next_token], dim=-1)
